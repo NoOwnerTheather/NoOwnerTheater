e02be3f79ea1c647c799601aa9b51ac9c2eaf9b3
wooyoung0830 <thddndud0830@gmail.com>
2022-02-06 15:03:57 +0900

    [FEAT] business_list, business_detail, navbar

--- theater/urls.py
+++ theater/urls.py
@@ -5,4 +5,6 @@ app_name = 'theater'
 
 urlpatterns = [
    path('', views.main, name='main'),
+   path('business_list/', views.business_list, name="business_list"),
+   path('<int:pk>/business/', views.business_detail, name="business_detail"),
 ]
