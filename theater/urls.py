34f3912b0af747adeec57d331f46a2f49e8033ad
hayounsong <fishingest@kookmin.ac.kr>
2022-02-07 12:59:30 +0900

    [FEAT]Movie Form

--- theater/urls.py
+++ theater/urls.py
@@ -5,4 +5,5 @@ app_name = 'theater'
 
 urlpatterns = [
    path('', views.main, name='main'),
+   path('movie_enroll/',views.movie_enroll,name='enroll'),
 ]
--- theater/views.py
+++ theater/views.py
@@ -1,5 +1,25 @@
+from re import template
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
-
+from .forms import MovieForm
 def main(request):
    return render(request, template_name='theater/main.html')
+
+
+
+def movie_enroll(request):
+   
+   if request.method=="POST":
+      form=MovieForm(request.POST,request.FILES)
+
+      if form.is_valid():
+         post=form.save()
+
+         return redirect('theater:main')
+
+   else: 
+      form=MovieForm()
+      ctx={'form':form}
+      return render(request,template_name='theater/enroll.html',context=ctx)
+
+
