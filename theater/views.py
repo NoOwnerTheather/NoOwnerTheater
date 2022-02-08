15a808f6d0138e603e154a07081cb6d5f62ca522
Yujin Lee <hojen5589@naver.com>
2022-02-08 12:12:05 +0900

    [FEAT] delete comment

--- theater/views.py
+++ theater/views.py
@@ -41,36 +41,46 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
-def like_ajax(request):
+def like_ajax(request,pk):
     req=json.loads(request.body) # json 임퐅, {'id':1, 'type':'like'} 파이썬 딕셔너리 형태로 변환하여 req변수에 담아준다
     post_id=req['id']
     button_type=req['type']
 
-    movie=Movie.objects.get(id=post_id) #id가 1인 포스트를 찾아서 변수에 넣어준다
+    comment=CommentPreview.objects.get(id=post_id) #id가 1인 포스트를 찾아서 변수에 넣어준다
 
-    movie.save()
+    if button_type=='like':
+        comment.like+=1
+
+    else:
+        comment.like-=1
+
+
+    comment.save()
 
     return JsonResponse({'id':post_id, 'type':button_type})
 
 
-@csrf_exempt
-def write_comment(request):
-    req = json.loads(request.body)
-    id = req['id']
-    type = req['type']
-    content = req['content']
-
-    movie = Movie.objects.get(id=id)
-    comment = CommentPreview.objects.create(movie=movie, content=content)
-    comment.save()
-    return JsonResponse({'id': id, 'type': type, 'content': content, 'comment_id': comment.id})
-
 
 @csrf_exempt
-def del_comment(request):
-    req = json.loads(request.body)
-    comment_id = req['id']
+def write_comment(request,pk):
+   print("hi")
+   req = json.loads(request.body)
+   id = req['id']
+   type = req['type']
+   content = req['content']
+   user=req['user']
 
-    comment = get_object_or_404(CommentPreview, id=comment_id)
-    comment.delete()
-    return JsonResponse({'id': comment_id})
+   movie = Movie.objects.get(id=id)
+   comment = CommentPreview.objects.create(movie=movie, content=content, user=user)
+   print(comment)
+   comment.save()
+   return JsonResponse({'id': id, 'type': type, 'content': content, 'comment_id': comment.id})
+
+
+@csrf_exempt
+def del_comment(request,pk):
+   req = json.loads(request.body)
+   comment_id = req['id']
+   comment = get_object_or_404(CommentPreview, id=comment_id)
+   comment.delete()
+   return JsonResponse({'id': comment_id})
